0ddce917b9b71be674895e7c482667f2c1740d31
DanielParra <danielparra05@outlook.com>
2019-06-12 21:37:45 -0500

    Adicion de los reportes impresos

--- src/app/ordenes/orden.component.ts
+++ src/app/ordenes/orden.component.ts
@@ -4,6 +4,7 @@ import { OrdenService } from './orden.service';
 import swal from 'sweetalert2';
 import * as jsPDF from 'jspdf';
 import { Ajustes } from '../ajustes/ajustes';
+import { AjusteService } from '../ajustes/ajustes.service';
 declare var $: any;
 
 @Component({
@@ -16,7 +17,7 @@ export class OrdenComponent implements OnInit {
   public ajustes: Ajustes;
   public ordenSalida: Orden = new Orden();
 
-  constructor(private ordenService: OrdenService) {
+  constructor(private ordenService: OrdenService, private ajustesService: AjusteService) {
   }
 
   /**
@@ -29,6 +30,7 @@ export class OrdenComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.ajustesService.getAjustes().subscribe((ajustes) => this.ajustes = ajustes);
     this.ordenService.getOrdenes().subscribe(
       ordenes => this.ordenes = ordenes
     );
@@ -51,19 +53,56 @@ export class OrdenComponent implements OnInit {
     const doc = new jsPDF();
     if (ordenSeleccionada.estado === 'EN_REPARACION') {
       doc.setFontSize(23);
-      doc.text(10, 9, 'Herramientas Eléctricas');
+      doc.text(10, 9, (this.ajustes.txtEncabezado != null) ? this.ajustes.txtEncabezado : 'No Data');
       doc.setFontSize(10);
-      doc.setFontType("bold");
+      doc.setFontType('bold');
       doc.text(150, 5, 'Orden de Servicio: ' + ordenSeleccionada.id);
       doc.text(150, 10, 'Fecha de Entrada: ' + ordenSeleccionada.fechaEntrada);
       doc.line(10, 12, 200, 12);
-      doc.setFontType("normal");
+      doc.setFontType('normal');
       doc.text(10, 17, 'Nombre del cliente: ' + ordenSeleccionada.cliente.nombre + ' ' + ordenSeleccionada.cliente.apellido);
       doc.text(150, 17, 'Telefono del cliente: ' + ordenSeleccionada.cliente.telefono);
       doc.line(10, 20, 200, 20);
+      doc.text(10, 25, 'Articulo: ' + ordenSeleccionada.nombreArticulo);
+      doc.text(120, 25, 'Modelo: ' + ordenSeleccionada.modeloArticulo);
+      doc.text(10, 30, 'Marca: ' + ordenSeleccionada.marcaArticulo);
+      doc.text(120, 30, 'Serie: ' + ordenSeleccionada.serialArticulo);
+      doc.line(10, 35, 200, 35);
+      doc.text(10, 40, 'Problema reportado: ' + ordenSeleccionada.problemaReportado);
+      doc.text(10, 45, 'Observaciones: ' + (ordenSeleccionada.observaciones != null ?
+                        ordenSeleccionada.observaciones : 'No Observaciones') );
+      doc.text(10, 50, 'Valor del Arreglo: ' + (ordenSeleccionada.valorArreglo != null ?
+              ('$' + ordenSeleccionada.valorArreglo) : 'No definido aún'));
+      doc.line(10, 51, 200, 51);
+      doc.setFontType('bold');
+      doc.text(10, 56, (this.ajustes.txtPiePagina != null) ? this.ajustes.txtPiePagina : 'No Data');
       doc.save('constanteOrden_' + ordenSeleccionada.id + '.pdf');
     } else {
-      doc.text('Epa colombia', 10, 10);
+      doc.setFontSize(23);
+      doc.text(10, 9, (this.ajustes.txtEncabezado != null) ? this.ajustes.txtEncabezado : 'No Data');
+      doc.setFontSize(10);
+      doc.setFontType('bold');
+      doc.text(150, 5, 'Orden de Servicio: ' + ordenSeleccionada.id);
+      doc.text(150, 10, 'Fecha de Salida: ' + ordenSeleccionada.fechaSalida);
+      doc.line(10, 12, 200, 12);
+      doc.setFontType('normal');
+      doc.text(10, 17, 'Nombre del cliente: ' + ordenSeleccionada.cliente.nombre + ' ' + ordenSeleccionada.cliente.apellido);
+      doc.text(150, 17, 'Telefono del cliente: ' + ordenSeleccionada.cliente.telefono);
+      doc.line(10, 20, 200, 20);
+      doc.text(10, 25, 'Articulo: ' + ordenSeleccionada.nombreArticulo);
+      doc.text(120, 25, 'Modelo: ' + ordenSeleccionada.modeloArticulo);
+      doc.text(10, 30, 'Marca: ' + ordenSeleccionada.marcaArticulo);
+      doc.text(120, 30, 'Serie: ' + ordenSeleccionada.serialArticulo);
+      doc.line(10, 35, 200, 35);
+      doc.text(10, 40, 'Problema reportado: ' + ordenSeleccionada.problemaReportado);
+      doc.text(10, 45, 'Observaciones: ' + (ordenSeleccionada.observaciones != null ?
+              ordenSeleccionada.observaciones : 'No Observaciones') );
+      doc.text(10, 50, 'Valor del Arreglo: ' + (ordenSeleccionada.valorArreglo != null ?
+              ('$' + ordenSeleccionada.valorArreglo) : 'No definido aún'));
+      doc.line(10, 51, 200, 51);
+      doc.setFontType('bold');
+      doc.setFontSize(9);
+      doc.text(10, 56, (this.ajustes.txtPiePagina != null) ? this.ajustes.txtPiePagina : 'No Data');
       doc.save('salidaOrden_' + ordenSeleccionada.id + '.pdf');
     }
   }
