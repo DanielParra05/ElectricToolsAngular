b0de66c95650a1b516f523435fd350effe0fea2c
DanielParra <danielparra05@outlook.com>
2019-08-12 13:01:02 -0500

    Cambio en el header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-
 })
 
 export class HeaderComponent {
-  variable = 'Herramientas Eléctricas';
+  titulo = 'Herramientas Eléctricas';
 }
